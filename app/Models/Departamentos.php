afc199915bf56cc5f856ddd6f31baf2893e338ef
noemvy <nvrm726@gmail.com>
2024-10-28 16:21:11 -0500

    Update: estableciendo relaciones

--- app/Models/Departamentos.php
+++ app/Models/Departamentos.php
@@ -4,15 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Empleados;
+use App\Models\Teams;
 
 class Departamentos extends Model
 {
     use HasFactory;
 
-     protected $table = 'departamentos';
+    protected $table = 'departamentos';
     protected $fillable = [
         'codigo',
         'nombre',
         'descripcion',
     ];
+
+
+    public function empleados()
+{
+    return $this->hasMany(Empleados::class,'codigo_departamento','codigo');
+
+
+}
+
+public function teams(){
+    return $this->hasMany(Teams::class,'codigo_departamento','codigo');
+}
+
+
 }
