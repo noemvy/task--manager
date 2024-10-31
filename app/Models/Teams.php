3d84335704f9ff3028ca22ee5cd510e010ec01ad
noemvy <nvrm726@gmail.com>
2024-10-31 00:41:30 -0500

    Update: Estableciendo relaciones con los modelos Empleado y Departamento

--- app/Models/Teams.php
+++ app/Models/Teams.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Teams extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'codigo_departamento',
+        'codigo',
+        'nombre',
+        'cedula_empleado',
+        'descripcion',
+    ];
+
+    //Relacion con el modelo empleados
+    public function empleados(){
+        return $this->HasMany(Empleados::class,'cedula');
+    }
+    public function departamento(){
+        return $this->belongsTo(Departamentos::class,'codigo');
+    }
 }
