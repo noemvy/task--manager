1d5b29b355be3ae0a610988727c86e947f993c69
noemvy <nvrm726@gmail.com>
2024-10-31 21:13:34 -0500

    Update: Especificaciones de relaciones con el modelo Teams, para la tabla pivote

--- app/Models/Empleados.php
+++ app/Models/Empleados.php
@@ -7,13 +7,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Departamentos;
 use App\Models\Teams;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Empleados extends Model
 {
     use HasFactory;
+    protected $table = 'empleados';
+    protected $primaryKey = 'cedula';
+    public $incrementing = false;
+    protected $keyType = 'string';
     // Permitir la asignación masiva para estos campos
     protected $fillable = [
         'codigo_departamento',
+        'codigo_team',
         'cedula',
         'nombre',
         'apellido',
@@ -27,4 +33,11 @@ class Empleados extends Model
     {
         return $this->belongsTo(Departamentos::class,'codigo_departamento','codigo');
     }
+       // Relación con el modelo Teams
+       public function teams(): BelongsToMany
+    {
+        return $this->belongsToMany(Teams::class, 'team_empleado', 'cedula', 'codigo')
+                    ->withTimestamps();
+    }
+
 }
