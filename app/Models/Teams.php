1516261a090de4567e8d8306bd0730e33419f41b
noemvy <nvrm726@gmail.com>
2024-10-31 21:14:14 -0500

    Update: Especificaciones de relaciones con el modelo Empleado con la tabla pivote

--- app/Models/Teams.php
+++ app/Models/Teams.php
@@ -4,24 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Teams extends Model
 {
     use HasFactory;
 
+    protected $table = 'teams';
+    protected $primaryKey = 'codigo';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+
     protected $fillable = [
         'codigo_departamento',
         'codigo',
         'nombre',
-        'cedula_empleado',
         'descripcion',
     ];
 
     //Relacion con el modelo empleados
-    public function empleados(){
-        return $this->HasMany(Empleados::class,'cedula');
+    public function empleados(): BelongsToMany
+    {
+        return $this->belongsToMany(Empleados::class, 'team_empleado', 'codigo', 'cedula')
+                    ->withTimestamps();
     }
     public function departamento(){
-        return $this->belongsTo(Departamentos::class,'codigo');
+        return $this->belongsTo(Departamentos::class,'codigo_departamento','codigo');
     }
 }
