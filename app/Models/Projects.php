7a034190bf4320e7d9f33ca8d35c0f3ea9225eb8
noemvy <nvrm726@gmail.com>
2024-11-03 21:22:57 -0500

    Update: Creación de relaciones con el modelo de Teams

--- app/Models/Projects.php
+++ app/Models/Projects.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Projects extends Model
 {
     use HasFactory;
+    protected $table = 'projects';
+    protected $fillable = [
+        'codigo_team',
+        'codigo_proyecto',
+        'nombre',
+        'descripcion',
+        'status',
+        'prioridad',
+        'fecha_inicio',
+        'fecha_finalizacion',
+
+    ];
+
+    public function teams(){
+        return $this ->belongsTo(Teams::class,'codigo_team','codigo');
+    }
+
 }
