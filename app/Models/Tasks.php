b70b1b2cefbe359b3aa25703258cf8ad2d88d172
noemvy <nvrm726@gmail.com>
2024-11-04 17:07:44 -0500

    Update: Agregando relaciones con projects

--- app/Models/Tasks.php
+++ app/Models/Tasks.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Tasks extends Model
 {
     use HasFactory;
+    protected $table = 'tasks';
+    protected $fillable = [
+        'codigo_proyecto',
+        'codigo',
+        'nombre',
+        'descripcion',
+        'status',
+        'prioridad',
+        'fecha_inicio',
+        'fecha_finalizacion',
+    ];
+
+
+
+    public function projects (){
+        return $this ->belongsTo(Projects::class, 'codigo_proyecto','codigo');
+    }
 }
