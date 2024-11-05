bccc6e3024b61902812b9bf91e78e5230b027b5c
noemvy <nvrm726@gmail.com>
2024-11-05 00:11:03 -0500

    Update: Agregando acciones en las vista de tabla

--- app/Filament/Resources/DepartamentosResource.php
+++ app/Filament/Resources/DepartamentosResource.php
@@ -55,6 +55,7 @@ class DepartamentosResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
